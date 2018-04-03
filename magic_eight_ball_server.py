d81403bdaf4332898d98d6369113b8c46565ee5d
Lindberg <paul.lindberg@CHAMPLAIN.EDU>
2018-04-03 15:23:45 -0400

    Updated Server but you need to edit itgit add .

--- magic_eight_ball_server.py
+++ magic_eight_ball_server.py
@@ -56,3 +56,31 @@ class EightBallServer(asyncio.Protocol):
                   .format(self.address, self.data))
         else:
             print('Client {} closed socket'.format(self.address))
+
+
+if __name__ == '__main__':
+    #EDIT THIS PASTED IN CODE
+    address = zen_utils.parse_command_line('asyncio server using callbacks')
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(ZenServer, *address)
+    server = loop.run_until_complete(coro)
+    print('Listening at {}'.format(address))
+    try:
+        loop.run_forever()
+    finally:
+        server.close()
+        loop.close()
+
+    #EDIT THIS PASTED IN CODE
+    parser = argparse.ArgumentParser(description='Example client')
+    parser.add_argument('host', help='IP or hostname')
+    parser.add_argument('-p', metavar='port', type=int, default=7000,
+                        help='TCP port (default 7000)')
+    parser.add_argument('-t', action='store_true', help='test mode')
+    parser.add_argument('-n', metavar='num threads', type=int, default=4,
+                        help='Num threads for test mode')
+    args = parser.parse_args()
+    if args.t:
+        test(args.host, args.p, args.n)
+    else:
+        run_interactive_client(args.host, args.p)
